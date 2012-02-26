d9f1ddf8765ceeb8e870d40d880704311f7a9666
Robin Clowers <robin.clowers@gmail.com>
2012-02-26 07:58:34 -0800

    pointing to github pages

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/robinclowers/"
+http_images_path = "/robinclowers/images"
+http_fonts_path = "/robinclowers/fonts"
+css_dir = "public/robinclowers/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
